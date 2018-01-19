bb0d7da3922e14f5daaf4fdd86ce8d05b702ef33
Savion-NEU <35351608+Savion-NEU@users.noreply.github.com>
2018-01-18 21:46:27 -0500

    Eighth Commit
    First initial and not final stick figure

---
+++ assignment 1/sketch.js
@@ -0,0 +1,16 @@
+function setup() {
+  createCanvas(500, 500);
+  background(0);
+  noStroke();
+
+  fill(204);
+  ellipse(250, 250, 72, 72)
+  rect(225, 275, 50, 75)
+  rect(125, 275, 100, 10)
+  rect(275, 275, 100, 10)
+  rect(225, 350, 10, 100)
+  rect(265, 350, 10, 100)
+}
+
+
+
